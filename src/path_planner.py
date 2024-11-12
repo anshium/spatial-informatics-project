660a102b3f4ff8f2b52194bb8d2d98d3312db712
anshium <anshchablani@gmail.com>
2024-11-12 23:35:49 +0530

    feat: image successfully loaded

--- src/path_planner.py
+++ src/path_planner.py
@@ -1,4 +1,12 @@
 from collections import defaultdict
+import heapq
+
+from osgeo import gdal 
+
+from PIL import Image
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 class Graph:
     def __init__(self, connections, directed=False):
@@ -31,19 +39,28 @@ class Graph:
         """Return the weight of the edge between node1 and node2, or None if no edge exists."""
         return self._graph[node1].get(node2)
 
-    def find_path(self, node1, node2, path=[]):
-        """Find any path between node1 and node2 (may not be the shortest)."""
-        path = path + [node1]
-        if node1 == node2:
-            return path
-        if node1 not in self._graph:
-            return None
-        for node in self._graph[node1]:
-            if node not in path:
-                new_path = self.find_path(node, node2, path)
-                if new_path:
-                    return new_path
-        return None
+    def find_shortest_path(self, start, end):
+        """Find the shortest path from start to end using Dijkstra's algorithm."""
+        priority_queue = [(0, [start])]
+        shortest_distances = {start: 0}
+
+        while priority_queue:
+            current_cost, path = heapq.heappop(priority_queue)
+            current_node = path[-1]
+
+            if current_node == end:
+                return path, current_cost
+
+            # explore neighbors
+            for neighbor, weight in self._graph[current_node].items():
+                new_cost = current_cost + weight
+                
+                # only consider new path if it is better
+                if neighbor not in shortest_distances or new_cost < shortest_distances[neighbor]:
+                    shortest_distances[neighbor] = new_cost
+                    heapq.heappush(priority_queue, (new_cost, path + [neighbor]))
+
+        return None, float('inf')
 
     def __str__(self):
         return '{}({})'.format(self.__class__.__name__, dict(self._graph))
@@ -55,16 +72,55 @@ class Graph:
             print(f"{node} --> {connections}")
 
 
+def test_graph():
+    sample_graph = Graph(((1, 2, 3),))
+    
+    sample_graph.add(4, 7, 4)
+    sample_graph.add(9, 7, 5)
+    
+    sample_graph.print_graph()
+    
+
 def path_planner():
     
-    test_graph = Graph(((1, 2, 3),))
+    # Open the dtm as a raster image
+    DATASETS_PATH = "datasets"
     
-    test_graph.add(4, 7, 4)
-    test_graph.add(9, 7, 5)
+    dtm_path_tiff = f"{DATASETS_PATH}/HiRISE/hello.tiff"
+
+    dtm_image = gdal.Open(dtm_path_tiff)
     
-    test_graph.print_graph()
+    band1 = dtm_image.GetRasterBand(1) # Red channel 
+    band2 = dtm_image.GetRasterBand(1) # Green channel 
+    band3 = dtm_image.GetRasterBand(1) # Blue channel
     
-    # Open the dem as a raster
+    b1 = band1.ReadAsArray() 
+    b2 = band2.ReadAsArray() 
+    b3 = band3.ReadAsArray() 
+    
+    dtm_image_array = np.array(dtm_image)
+    
+    print(np.max(dtm_image_array))
+
+
+    img = np.dstack((b1, b2, b3)) 
+
+    min_val = -3961.39
+    max_val = -3927.94
+    
+    img_clipped = np.clip(img, min_val, max_val)
+    
+    img_normalized = ((img_clipped - min_val) / (max_val - min_val) * 255).astype(np.uint8)
+    
+    x_start, y_start = 1000, 1000
+    x_end, y_end = 4000, 7000
+    img_cropped = img_normalized[y_start:y_end, x_start:x_end]
+    
+    print(np.mean(img_normalized))
+    
+    plt.imshow(img_cropped)
+    plt.savefig('Cropped_Tiff.png')
+    plt.show()
     
     
     # Make a graph of the raster with costs based on just the elevation values
